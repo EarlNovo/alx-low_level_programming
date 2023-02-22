ee77c7349577eb5151780895a9e342fd665cf84a
EarlNovo <efenuel2000@gmail.com>
2023-02-22 14:36:02 -0800

    task 0-5

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ *
+ *  Return: Always 0 (sucess)
+ */
+void print_alphabet(void)
+{	
+	char letter;
+	
+	for(letter ='a'; lette< ='z'; letter ++)
+	_putchar(letter);
+	_putchar('\n');
+}
